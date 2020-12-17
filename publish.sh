8b8f698a8af95964d464402fa2cc9b7b2206df04
Siyuan Peng <siyuan.kevin.peng@gmail.com>
2020-12-17 17:53:22 -0500

    create new jupyter

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-jupyter nbconvert game_sale_analysis.ipynb --to markdown --output "README.md"
-git add README.md game_sale_analysis.ipynb
+jupyter nbconvert game_sale.ipynb --to markdown --output "README.md"
+git add README.md game_sale.ipynb
 git commit -m "generate publish file"
 git push
